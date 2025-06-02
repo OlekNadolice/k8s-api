edea17172b7c7175cae070814efd23dcc46bb9d2
OlekNadolice <cygan.aleksander18@gmail.com>
2025-06-02 19:24:52 +0200

    Add enum

--- src/api/cluster-controller.ts
+++ src/api/cluster-controller.ts
@@ -1,9 +1,22 @@
 import { getRouter } from "./config";
+import { HTTP_STATUSES } from "./config";
 
 export const clusterController = getRouter()
 
 
+// returns clusters assigned to the user based on ID FROM JWT
+clusterController.get('', (req, res) => {
+  const mockedObject = [
+    {
+      name: 'EKS',
+      server: "http://localhost",
+      status: 'connected',
+    }
+  ]
+  res.status(HTTP_STATUSES.OK).json(mockedObject)
+})
 
-clusterController.get('', (req,res) => {
-  res.send('heloo friend')
+// returns details about a cluster based on ID
+clusterController.get('/:clusterId', (req, res ) => {
+
 })
